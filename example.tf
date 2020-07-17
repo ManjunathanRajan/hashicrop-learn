0b86ab73354d4044037017a3c89b4220d79578d0
Manjunathan Rajan <32861102+ManjunathanRajan@users.noreply.github.com>
2020-07-17 08:31:39 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -7,8 +7,3 @@ resource "aws_instance" "example" {
   ami           = "ami-0b1e2eeb33ce3d66f"
   instance_type = "t2.micro"
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-getting-started-guide"
-  acl    = "private"
-}
